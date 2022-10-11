5112327384e7bd1a8f9d9acaec0ea8adf1e6a02e
Pontus <pontusbergqvist97@gmail.com>
2022-10-11 23:00:25 +0200

    changed button text

--- pages/products/[product].js
+++ pages/products/[product].js
@@ -1,6 +1,6 @@
 import { Image, StructuredText } from "react-datocms";
 import { gql } from "graphql-request";
-import request from '../../utils/request';
+import request from "../../utils/request";
 import responsiveImage from "../../utils/responsiveImage";
 
 export const getStaticPaths = async () => {
@@ -15,14 +15,15 @@ export const getStaticPaths = async () => {
   const res = await request({ query });
   const paths = res.allProducts.map((product) => {
     return {
-      params: { product: product.id }
-    }});
+      params: { product: product.id },
+    };
+  });
 
   return {
     paths,
     fallback: false,
   };
-}
+};
 
 export const getStaticProps = async ({ params }) => {
   const query = gql`
@@ -49,30 +50,36 @@ export const getStaticProps = async ({ params }) => {
   const res = await request({ query });
   const product = res.allProducts[0];
   return {
-    props: { product }
-  }
-}
+    props: { product },
+  };
+};
 
 const Product = ({ product }) => {
-  console.log(product)
   return (
     <div className="flex laptop:flex-row flex-col">
       <div className="laptop:w-1/2 w-full grid grid-cols-2">
-          <Image data={product.mainImage.responsiveImage} className="col-span-2" />
-          {product.alternativeImages.map(image => <Image data={image.responsiveImage} />)}
+        <Image
+          data={product.mainImage.responsiveImage}
+          className="col-span-2"
+        />
+        {product.alternativeImages.map((image, index) => (
+          <Image data={image.responsiveImage} key={index} />
+        ))}
       </div>
       <div className="px-5">
         <div>
           <h2 className="text-[30px]">{product.name}</h2>
           <h3 className="text-[25px]">{`${product.price}:-`}</h3>
-          <button className="bg-blue-500 text-white p-1 px-2 rounded">Lägg till</button>
+          <button className="bg-blue-500 text-white p-1 px-2 rounded">
+            Add to cart
+          </button>
         </div>
         <div className="max-w-[600px] structured-text">
           <StructuredText data={product.description} />
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default Product;
